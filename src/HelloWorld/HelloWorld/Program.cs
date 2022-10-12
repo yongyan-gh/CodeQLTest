7c8caa4183850e6275448d91ee1af497aa0e0e37
Yong Yan <yanyong-gh@outlook.com>
2022-10-12 13:33:10 -0700

    first draft

---
+++ src/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SymmetricAlgorithm serviceProvider = new DESCryptoServiceProvider();
+            byte[] key = { 16, 22, 240, 11, 18, 150, 192, 21 };
+            serviceProvider.Key = key;
+            ICryptoTransform encryptor = serviceProvider.CreateEncryptor();
+
+            String message = "Hello World";
+            byte[] messageB = System.Text.Encoding.ASCII.GetBytes(message);
+            var encrypted = encryptor.TransformFinalBlock(messageB, 0, messageB.Length);
+        }
+    }
+}
